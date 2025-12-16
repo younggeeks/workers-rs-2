903a0802daed163b09235fa50f19d77b71523c94
Samwel Charles <younggeeks101@gmail.com>
2025-12-16 19:02:49 +0300

    refactor: remove Axum and Leptos template files and configurations

--- templates/leptos/src/main.rs
+++
@@ -1,5 +0,0 @@
-pub fn main() {
-    // no client-side main function
-    // unless we want this to work with e.g., Trunk for pure client-side testing
-    // see lib.rs for hydration function instead
-}
